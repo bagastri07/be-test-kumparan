4be2d31426e54fc1fa959002b503fd51992c99f4
Bagas Tri Wibowo <bagas.tri@tanihub.com>
2022-07-27 02:53:11 +0700

    [ADD] unit test get author by id repository

--- services/api/author/author_repository_test.go
+++ services/api/author/author_repository_test.go
@@ -81,3 +81,94 @@ func Test_authorRepository_InsertAuthor(t *testing.T) {
 		})
 	}
 }
+
+func Test_authorRepository_GetAuthorByID(t *testing.T) {
+	type args struct {
+		ctx      context.Context
+		authorID string
+	}
+
+	type mockGet struct {
+		data *models.Author
+		err  error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		mockGet mockGet
+		want    *models.Author
+		wantErr error
+	}{
+		{
+			name: "when success, then return data",
+			args: args{
+				ctx:      context.TODO(),
+				authorID: "123",
+			},
+			mockGet: mockGet{
+				data: &models.Author{
+					ID:   "123",
+					Name: "Siful",
+				},
+				err: nil,
+			},
+			want: &models.Author{
+				ID:   "123",
+				Name: "Siful",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "when err, then return err",
+			args: args{
+				ctx:      context.TODO(),
+				authorID: "123",
+			},
+			mockGet: mockGet{
+				data: nil,
+				err:  sql.ErrConnDone,
+			},
+			want:    nil,
+			wantErr: sql.ErrConnDone,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := NewRepository()
+
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+			mockExpectGet := mock.ExpectQuery(
+				`SELECT id FROM authors WHERE deleted_at IS NULL AND id = ?`,
+			)
+
+			if tt.mockGet.err != nil {
+				mockExpectGet.WillReturnError(tt.mockGet.err)
+			} else {
+				row := sqlmock.NewRows([]string{
+					"id",
+					"name",
+					"created_at",
+					"updated_at",
+				})
+
+				row.AddRow(
+					tt.mockGet.data.ID,
+					tt.mockGet.data.Name,
+					tt.mockGet.data.CreatedAt,
+					tt.mockGet.data.UpdatedAt,
+				)
+				mockExpectGet.WillReturnRows(row)
+			}
+
+			data, err := repo.GetAuthorByID(tt.args.ctx, sqlxDB, tt.args.authorID)
+			assert.Equal(t, tt.want, data)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
