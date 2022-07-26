58c7ec087019e0b493a9884f1c415b26091212bd
Bagas Tri Wibowo <bagas.tri@tanihub.com>
2022-07-27 01:38:18 +0700

    [ADD] unit test get list article repository

--- services/api/article/article_repository_test.go
+++ services/api/article/article_repository_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/bagastri07/be-test-kumparan/models"
+	"github.com/bagastri07/be-test-kumparan/models/base_models"
 	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
 )
@@ -176,6 +178,136 @@ func Test_articleRepository_GetCountArticle(t *testing.T) {
 			}
 
 			data, err := repo.GetCountArticle(tt.args.ctx, sqlxDB, tt.args.filter)
+			assert.Equal(t, tt.want, data)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func Test_articleRepository_GetArticlesList(t *testing.T) {
+	type args struct {
+		ctx    context.Context
+		filter *models.ArticleFilter
+	}
+
+	type mockSelect struct {
+		data []models.Article
+		err  error
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		mockSelect mockSelect
+		want       []models.Article
+		wantErr    error
+	}{
+		{
+			name: "when success, then return data",
+			args: args{
+				ctx:    context.TODO(),
+				filter: &models.ArticleFilter{},
+			},
+			mockSelect: mockSelect{
+				data: []models.Article{
+					{
+						ID:         "1234",
+						AuthorID:   "2345",
+						AuthorName: "Jamal",
+						Title:      "Judul Kumparan",
+						Body:       "Body Kumparan",
+						BaseTimestamp: base_models.BaseTimestamp{
+							CreatedAt: &time.Time{},
+							UpdatedAt: &time.Time{},
+						},
+					},
+				},
+				err: nil,
+			},
+			want: []models.Article{
+				{
+					ID:         "1234",
+					AuthorID:   "2345",
+					AuthorName: "Jamal",
+					Title:      "Judul Kumparan",
+					Body:       "Body Kumparan",
+					BaseTimestamp: base_models.BaseTimestamp{
+						CreatedAt: &time.Time{},
+						UpdatedAt: &time.Time{},
+					},
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "when err, then return err",
+			args: args{
+				ctx:    context.TODO(),
+				filter: &models.ArticleFilter{},
+			},
+			mockSelect: mockSelect{
+				data: nil,
+				err:  sql.ErrConnDone,
+			},
+			want:    nil,
+			wantErr: sql.ErrConnDone,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := NewRepository()
+
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+			mockExpectSelect := mock.ExpectQuery(
+				`SELECT
+					articles.id,
+					articles.author_id,
+					articles.title,
+					articles.body,
+					articles.created_at,
+					articles.updated_at,
+					articles.deleted_at,
+					authors.name AS author_name
+				FROM
+					articles
+				JOIN
+					authors ON authors.id = articles.author_id
+				WHERE articles.deleted_at IS NULL ORDER BY created_at DESC LIMIT 0`,
+			)
+
+			if tt.mockSelect.err != nil {
+				mockExpectSelect.WillReturnError(tt.mockSelect.err)
+			} else {
+				rows := sqlmock.NewRows([]string{
+					"id",
+					"author_id",
+					"author_name",
+					"title",
+					"body",
+					"created_at",
+					"updated_at",
+				})
+				for _, article := range tt.mockSelect.data {
+					rows.AddRow(
+						article.ID,
+						article.AuthorID,
+						article.AuthorName,
+						article.Title,
+						article.Body,
+						article.CreatedAt,
+						article.UpdatedAt,
+					)
+				}
+				mockExpectSelect.WillReturnRows(rows)
+			}
+
+			data, err := repo.GetArticlesList(tt.args.ctx, sqlxDB, tt.args.filter)
 			assert.Equal(t, tt.want, data)
 			assert.Equal(t, tt.wantErr, err)
 		})
